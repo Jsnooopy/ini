5c3cc7535a62f85646621523d91743a4a628eabd
Jae <lijae3552@gmail.com>
2026-01-05 00:11:29 +0900

    first commit

---
+++ src/test/java/com/example/ini/IniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
